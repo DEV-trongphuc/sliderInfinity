1f1cb0c48a0c93ebf9443179fea339e06c7d51a4
Rememberlibrary <91412382+Rememberlibrary@users.noreply.github.com>
2021-10-08 19:29:16 +0700

    ssssss

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
-$(document).ready(function () {
-  const images = $(".img__item");
-  $(".next__btn").on("click", function () {
-    let i = Math.floor(images[0].offsetLeft / images[0].offsetWidth);
-    if (i <= -(images.length - 1)) i = 1;
-    i = (i - 1) * 100;
-    images.css("left", `${i}%`);
-  });
-  $(".prev__btn").on("click", function () {
-    let i = Math.floor(images[0].offsetLeft / images[0].offsetWidth);
-    if (i === 0) i = -images.length;
-    i = (i + 1) * 100;
-    images.css("left", `${i}%`);
-  });
-});
+const $ = document.querySelectorAll.bind(document);
+const images = $(".slider__item");
+$(".next__btn")[0].onclick = () => {
+  let i = Math.floor(images[0].offsetLeft / images[0].offsetWidth);
+  if (i <= -(images.length - 1)) i = 1;
+  i = (i - 1) * 100;
+  images.forEach((image) => (image.style.left = i + "%"))};
+$(".prev__btn")[0].onclick = () => {
+  let i = Math.floor(images[0].offsetLeft / images[0].offsetWidth);
+  if (i === 0) i = -images.length;
+  i = (i + 1) * 100;
+  images.forEach((image) => (image.style.left = i + "%"))};
