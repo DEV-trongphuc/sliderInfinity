4c06e46c0432db0fd9fe41405025806eb1da06da
trongphuc3105 <90214784+trongphuc3105@users.noreply.github.com>
2021-10-08 20:22:46 +0700

    aaaaaa

--- main.js
+++ main.js
@@ -3,12 +3,23 @@ const images = $(".slider__item");
 $(".next__btn")[0].onclick = () => {
   let i = Math.floor(images[0].offsetLeft / images[0].offsetWidth);
   if (i <= -(images.length - 1)) i = 1;
-  i = (i - 1) * 100;
-  images.forEach((image) => (image.style.left = i + "%"));
+  i = i - 1;
+  images.forEach((image) => (image.style.left = i * 100 + "%"));
+  $(".dot__item.active")[0].classList.remove("active");
+  $(".dot__item")[-i].classList.add("active");
 };
 $(".prev__btn")[0].onclick = () => {
   let i = Math.floor(images[0].offsetLeft / images[0].offsetWidth);
   if (i === 0) i = -images.length;
-  i = (i + 1) * 100;
-  images.forEach((image) => (image.style.left = i + "%"));
+  i = i + 1;
+  images.forEach((image) => (image.style.left = i * 100 + "%"));
+  $(".dot__item.active")[0].classList.remove("active");
+  $(".dot__item")[-i].classList.add("active");
 };
+$(".dot__item").forEach((dot, index) => {
+  dot.onclick = () => {
+    $(".dot__item.active")[0].classList.remove("active");
+    dot.classList.add("active");
+    images.forEach((image) => (image.style.left = -index * 100 + "%"));
+  };
+});
